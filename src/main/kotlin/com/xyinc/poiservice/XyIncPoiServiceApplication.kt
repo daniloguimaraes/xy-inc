5e5bff62fe9a7538c5a29f2ec4856f488ebbee4d
daniloguimaraes <guimaraesdjl@gmail.com>
2020-07-26 19:39:00 -0300

    project scaffolding + git ignoring

---
+++ src/main/kotlin/com/xyinc/poiservice/XyIncPoiServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.xyinc.poiservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XyIncPoiServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<XyIncPoiServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/xyinc/poiservice/XyIncPoiServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xyinc.poiservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XyIncPoiServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
