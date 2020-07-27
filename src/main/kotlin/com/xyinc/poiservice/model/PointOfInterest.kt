c79603751e4d03d0681de63d3193510a2a29539c
daniloguimaraes <guimaraesdjl@gmail.com>
2020-07-26 21:12:42 -0300

    adding PointOfInterest.kt data class

---
+++ src/main/kotlin/com/xyinc/poiservice/model/PointOfInterest.kt
@@ -0,0 +1,10 @@
+package com.xyinc.poiservice.model
+
+/**
+ * Data class to represent a POI (Point of Interest).
+ *
+ * @author Danilo Guimaraes
+ */
+data class PointOfInterest(val id: Long?, val name: String, val xCoordinate: Int, val yCoordinate: Int) {
+
+}
