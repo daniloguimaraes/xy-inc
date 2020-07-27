444fa3a846c21e9dcb4c4ec97a970c6439ffec12
daniloguimaraes <guimaraesdjl@gmail.com>
2020-07-26 23:05:46 -0300

    transforming PointOfInterest.kt into a JPA entity

--- src/main/kotlin/com/xyinc/poiservice/model/PointOfInterest.kt
+++ src/main/kotlin/com/xyinc/poiservice/model/PointOfInterest.kt
@@ -1,10 +1,27 @@
 package com.xyinc.poiservice.model
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
 /**
  * Data class to represent a POI (Point of Interest).
  *
  * @author Danilo Guimaraes
  */
-data class PointOfInterest(val id: Long?, val name: String, val xCoordinate: Int, val yCoordinate: Int) {
+@Entity
+data class PointOfInterest(
+        @Id @GeneratedValue
+        var id: Long? = null,
+
+        @Column(nullable = false)
+        var name: String,
+
+        @Column(nullable = false)
+        var xCoordinate: Int,
+
+        @Column(nullable = false)
+        var yCoordinate: Int) {
 
 }
