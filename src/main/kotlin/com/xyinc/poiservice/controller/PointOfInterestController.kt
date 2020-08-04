63604797b97007ccdf6030a1a241dd113f50344e
daniloguimaraes <guimaraesdjl@gmail.com>
2020-08-04 01:33:52 -0300

    adding swagger annotations to controller and creating allCoordinates endpoint

--- src/main/kotlin/com/xyinc/poiservice/controller/PointOfInterestController.kt
+++ src/main/kotlin/com/xyinc/poiservice/controller/PointOfInterestController.kt
@@ -2,33 +2,60 @@ package com.xyinc.poiservice.controller
 
 import com.xyinc.poiservice.model.PointOfInterest
 import com.xyinc.poiservice.service.PointOfInterestService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
 import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.*
 import org.springframework.web.bind.annotation.*
+import java.lang.StringBuilder
 
 /**
  * Point of Interest REST Controller
  *
  * @author Danilo Guimaraes
  */
+@Api("Point of Interest endpoints")
 @RestController
 @RequestMapping("/api/poi")
 internal class PointOfInterestController(val pointOfInterestService: PointOfInterestService) {
 
+    @ApiOperation(value = "Returns all Points of Interest")
     @GetMapping
     fun findAll(): ResponseEntity<List<PointOfInterest>> {
-        return ResponseEntity.ok(pointOfInterestService.findAll());
+        return ok(pointOfInterestService.findAll());
     }
 
+    @ApiOperation(value = "Creates a new Point of Interest")
     @PostMapping
     fun add(@RequestBody poi: PointOfInterest): PointOfInterest {
         return pointOfInterestService.add(poi);
     }
 
+    @ApiOperation("Returns all nearby Points of Interest")
     @GetMapping(path= ["/nearby/{x}/{y}/{radius}"])
-    fun findAllNearby(@PathVariable(required = true) x: Int,
-                    @PathVariable(required = true) y: Int,
-                    @PathVariable radius: Int = 10): ResponseEntity<List<PointOfInterest>> {
-        return ResponseEntity.ok(pointOfInterestService.findAllNearby(x, y, radius));
+    fun findAllNearby(@PathVariable(required = true) @ApiParam("X Coordinate") x: Int,
+                      @PathVariable(required = true) @ApiParam("Y Coordinate") y: Int,
+                      @PathVariable @ApiParam(value = "Radius", defaultValue = "10") radius: Int = 10): ResponseEntity<List<PointOfInterest>> {
+        return ok(pointOfInterestService.findAllNearby(x, y, radius));
+    }
+
+    @ApiOperation(value = "Returns all Points of Interest pair(s) of coordinate(s)",
+            notes = "All Points of Interest in the form '(x, y)'. Created for debugging purposes only.")
+    @GetMapping(path = ["/all-coordinates"])
+    fun allCoordinates(): ResponseEntity<String> {
+        val allPointsOfInterest = pointOfInterestService.findAll()
+
+        if (allPointsOfInterest.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        val sb = StringBuilder()
+        allPointsOfInterest.forEach {
+            sb.append("(%d, %d)\n".format(it.xCoordinate, it.yCoordinate))
+        }
+
+        return ok(sb.toString())
     }
 
 }
